46faf2bb9d0db1cf6dabfcf9f7df8d2113e74d60
SeryiBaran <seryibaranminepy@yandex.ru>
2024-04-25 22:48:11 +0300

    fix: remove test of running code

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,4 @@
 import { describe, expect, it } from 'vitest'
-import { execa } from 'execa'
 import { generateScript, transpile } from '../src/functions'
 
 describe('functions', () => {
@@ -8,10 +7,5 @@ describe('functions', () => {
   })
   it('transpile', () => {
     expect(transpile).not.toThrow()
-  })
-  it('run', async () => {
-    const child = await execa('node one.js')
-
-    expect(child.exitCode).toBe(0)
   })
 })
